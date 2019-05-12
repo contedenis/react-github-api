486fe7a344a57d1d4fcfcb3185990da4ff0b2efd
contedenis <contedenis@outlook.com>
2019-05-12 15:27:57 -0300

    fix/update-styles-in-trending-repositories (#17)

--- src/components/TrendingRepositories/RepoItem/index.js
+++ src/components/TrendingRepositories/RepoItem/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import moment from 'moment';
+import { Popup } from 'semantic-ui-react';
+
 
 import './styles.scss';
 
@@ -9,13 +12,31 @@ function RepoItem({ repo }) {
       <div className="repo-item__container">
         <img className="repo-item__image" src={repo.owner.avatar_url} alt={repo.full_name} />
         <div className="repo-item__data">
-          <code>{repo.full_name}</code>
-          <code>{repo.description}</code>
-          <code>{repo.created_at}</code>
-          <code>{repo.updated_at}</code>
-          <code>{repo.stargazers_count}</code>
-          <code>{repo.forks_count}</code>
-          <a href={repo.html_url}>More info ...</a>
+          <Popup
+            content="Click here if you want to go to the repo"
+            inverted
+            position="bottom left"
+            trigger={<a className="repo-item__title" href={repo.html_url}>{repo.full_name}</a>}
+          />
+          <h5 className="repo-item__description">{repo.description}</h5>
+          <div className="repo-item__description-info-container">
+            <div className="repo-item__description-info">
+              <div className="repo-item__description-info__label">Stars:</div>
+              <h5 className="repo-item__description-info__data">{repo.stargazers_count}</h5>
+            </div>
+            <div className="repo-item__description-info">
+              <div className="repo-item__description-info__label">Forks:</div>
+              <h5 className="repo-item__description-info__data">{repo.forks_count}</h5>
+            </div>
+            <div className="repo-item__description-info">
+              <div className="repo-item__description-info__label">Created:</div>
+              <h5 className="repo-item__description-info__data">{` ${moment(repo.created_at).format('ddd, MMMM Do YYYY')}`}</h5>
+            </div>
+            <div className="repo-item__description-info">
+              <div className="repo-item__description-info__label">Updated:</div>
+              <h5 className="repo-item__description-info__data">{` ${moment(repo.updated_at).format('ddd, MMMM Do YYYY')}`}</h5>
+            </div>
+          </div>
         </div>
       </div>
     </div>
